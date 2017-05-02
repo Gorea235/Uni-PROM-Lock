1f7565711b7b16842e479c99b4c1d422455e6c3f
Luke Davies <lkdvs3@gmail.com>
2017-05-02 06:41:57 +0100

    Added more tests to Timeout testing

--- tests/test_timeout.py
+++ tests/test_timeout.py
@@ -2,22 +2,35 @@
 import unittest
 import timeout
 import threading
+import time
 
 
 class TimeoutTest(unittest.TestCase):
     def setUp(self):
-        self.tm = timeout.Timeout(2)
+        self.tm = timeout.Timeout(1)
 
     def tearDown(self):
         self.tm.cleanup()
 
     def test_init(self):
         self.assertTrue(isinstance(self.tm._timer, threading.Timer))
-        self.assertEqual(self.tm._timer.interval, 2)
+        self.assertEqual(self.tm._timer.interval, 1)
     
     def test_start(self):
         self.tm.start()
         self.assertTrue(self.tm.active)
+
+    def fired_tester_func(self):
+        self.fired = True
+
+    def test_fired(self):
+        self.fired = False
+        self.tm.elapsed.bind(self.fired_tester_func)
+        self.tm.start()
+        time.sleep(0.9)
+        self.assertFalse(self.fired)
+        time.sleep(0.11)
+        self.assertTrue(self.fired)
     
     def test_reset(self):
         self.tm.start()
